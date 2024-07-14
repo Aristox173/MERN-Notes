7dd242067eef5f6a63df058227eb253a65478767
Juan Aristizabal <juanaristi2001@gmail.com>
2024-07-14 16:39:13 -0500

    Add Note Cards Component

--- frontend/mern-notes/src/views/pages/Home/Home.jsx
+++ frontend/mern-notes/src/views/pages/Home/Home.jsx
@@ -1,9 +1,33 @@
+import NoteCard from "../../components/Cards/NoteCard";
 import Navbar from "../../components/Navbar/Navbar";
+import { MdAdd } from "react-icons/md";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Meeting on 7th April"
+            date="2021-09-15"
+            content="Meeting on 7th April Meeting on 7th April"
+            tags="#Meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {}}
+      >
+        <MdAdd className="text-[32px] text-white" />
+      </button>
     </>
   );
 };
